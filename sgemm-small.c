f01ffc5a2dc1b582693e4e4c542b7b6f6d867f5a
sanchitbareja <sanchitbareja@gmail.com>
2012-03-12 20:44:58 -0700

    converted intrinsics to floating point

--- sgemm-small.c
+++ sgemm-small.c
@@ -25,20 +25,20 @@ void transpose( int n, int blocksize, float *dst, float *src ) {
 }
 
 
-int mul_vectorized( int n, int *a )
+int mul_vectorized( int n, float *a )
 {
     /* WRITE YOUR VECTORIZED CODE HERE */
-    __m128i mul = _mm_setzero_si128();
-    __m128i* ai = (__m128*) a;
+    __m128f mul = _mm_setzero_si128();
+    __m128f* ai = (__m128*) a;
     int index = 0;
     int temp_index = 0;
     int mul1[4];
     for(index = 0; index < n/4*4; index += 4){
 	ai = (__m128*) a;
-	mul = _mm_mul_epi32(mul, _mm_loadu_si128(ai));
+	mul = _mm_mul_ps(mul, _mm_loadu_ps(ai));
 	a += 4;
     }
-    _mm_storeu_si128((__m128*) mul1, mul);
+    _mm_storeu_ps((__m128*) mul1, mul);
     int mul2 = mul1[0]+mul1[1]+mul1[2]+mul1[3];
     while(index < n){
 	mul2 += a[temp_index];
